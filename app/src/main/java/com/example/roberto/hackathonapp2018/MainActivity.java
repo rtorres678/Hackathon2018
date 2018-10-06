be4e3d59e36b7d62fcc0374c38d27913d51d9095
Roberto Torres <roberto.torres@mavs.uta.edu>
2018-10-06 18:17:30 -0500

    add activity_org_home_page and org_home_event_view

--- app/src/main/java/com/example/roberto/hackathonapp2018/MainActivity.java
+++ app/src/main/java/com/example/roberto/hackathonapp2018/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.roberto.hackathonapp2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v)
+    {
+        Intent intent = new Intent(this, orgHomePageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString() + editText2.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
